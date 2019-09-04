1b9b25ab18a9524f93ab23dc64ba52e60dbd7233
Joel <camacho-joel@hotmail.com>
2019-09-04 19:44:31 +0200

    inital commit

---
+++ react-employees/src/TableBody.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+class TableBody extends Component {
+  render() {
+    return (
+      <tbody>
+        <tr>
+          <td>Caterin</td>
+          <td>CEO</td>
+        </tr>
+        <tr>
+          <td>Callie</td>
+          <td>CTO</td>
+        </tr>
+        <tr>
+          <td>Jona</td>
+          <td>Contador</td>
+        </tr>
+        <tr>
+          <td>Nico</td>
+          <td>Investor</td>
+        </tr>
+      </tbody>
+    );
+  }
+}
+
+export default TableBody;
---
+++ react-employees/src/TableHeader.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class TableHeader extends Component {
+  render() {
+    return (
+      <thead>
+        <tr>
+          <th>Nombre</th>
+          <th>Titulo</th>
+        </tr>
+      </thead>
+    );
+  }
+}
+
+export default TableHeader;
